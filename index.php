faae368f31ed0da078f9d94aa6a9555d80093579
halfgrayhuman <halfgrayhuman@gmail.com>
2024-01-31 15:20:38 -0500

    boom

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<div>
-    <img src="" alt="">
-    <a href="#">ima link</a>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <img src="" alt="">
+        <a href="#">ima link</a>
+    </div>
+</body>
+</html>
