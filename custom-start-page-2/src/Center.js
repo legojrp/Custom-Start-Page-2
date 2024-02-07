c736145be1e83fe66050f6993b337db9685ae2c2
halfgrayhuman <halfgrayhuman@gmail.com>
2024-02-07 14:52:45 -0500

    search does the searching!! (nic learns about the horrors of react)

--- custom-start-page-2/src/Center.js
+++ custom-start-page-2/src/Center.js
@@ -1,11 +1,25 @@
 function Center (){
+    const handleSearch = (event) => { // handleSearch decides how to open the link
+        event.preventDefault(); // prevent form from submitting normally
+        const searchText = event.target.q.value; // the text that is submitted
+
+        if (searchText.startsWith('http')) { // if the link uses http or https (starts with http) assume it is a valid url
+            window.open(searchText, '_blank'); // open the text as a url in a new tab
+        } else if (searchText.includes('.')) { // if the text contains a . assume it has a domain, but no protocol
+            window.open(`https://${searchText}`, '_blank'); // open text with added https protocol in a new tab
+        } else { // if the text does not contain http or . assume it is a search
+            window.open(`https://www.google.com/search?q=${searchText}`, '_blank'); // does a google search
+        }
+    };
 
     return (
         <div className="text-center">
-            <form action="https://www.google.com/search"> {/* Google search form */}
-                <input type="text" placeholder="Search" id="q" name="q" autoFocus/>
-            </form> {/* In the future, we will change this so if it is a valid website, it points directly to the website instead of opening in google */}
+            <form onSubmit={handleSearch}> {/* when submitted, handleSearch decides how to open the link*/}
+                <input type="text" placeholder="Search" id="q" name="q" autoFocus />
+            </form>
         </div>
     );
+
 }
+
 export default Center;
