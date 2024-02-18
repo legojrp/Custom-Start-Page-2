ca53ecc89ba78f7d9f5af7ca33c4353e0355ed03
legojrp <legojrp@gmail.com>
2024-02-17 23:58:48 -0500

    php remake a bug

--- backend/backend_dev/signin.php
+++ backend/backend_dev/signin.php
@@ -15,15 +15,15 @@ $conn = DBConnect::withCredential($CREDENTIALS);
 $passwordhash = hash("sha256", $password . $username);
 
 
-$results =$conn->select("users", ["id"], "username = '$username' AND password = '$passwordhash'");
-
+$results = $conn->select("users", ["id"], "username = '$username' AND password = '$passwordhash'");
+$results = json_decode(json_encode($results), true);
 if (!empty($results)) {
     $token = hash("sha256", $username . time());
-    $conn->insert("tokens", ["id", "token"], ["'results[0][id]'", "'$token'"]);
-    echo json_encode(['token' => $token, 'status' => 200]);
+    $conn->insert("tokens", ["id", "token"], [$results[0]['id'], "'$token'"]);
+    echo json_encode(['token' => $token, 'status' => "success", 'id' => $results[0]['id']]);
 }
 else {
-    echo json_encode(['status' => 400]);
+    echo json_encode(['status' => "fail"]);
 }
 
 
