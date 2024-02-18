e7dd4042ec15ea871e4e42b075658df4ed0e7b91
legojrp <legojrp@gmail.com>
2024-02-17 22:53:10 -0500

    Update signup.php

--- backend/backend_dev/signup.php
+++ backend/backend_dev/signup.php
@@ -5,20 +5,19 @@ header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 require_once 'DBConnect.php';
 require_once "Credentials.php";
+$username = $_POST['username'];
+$password = $_POST['password'];
 
-$input = file_get_contents('php://input');
-$data = json_decode($input);
-
-$passwordhash = hash("sha256", $data->password . $data->username);
+$passwordhash = hash("sha256", $password . $username);
 
 $conn = DBConnect::withCredential($CREDENTIALS);
 
-$conn->insert("users", ["username", "password"], ["'$data->username'", "'$passwordhash'"]);
+$conn->insert("users", ["username", "password"], ["'$username'", "'$passwordhash'"]);
 
-$results = $conn->select("users", ["id"], "username = $data->username AND password = $passwordhash");
+$results = $conn->select("users", ["id"], "username = $username AND password = $passwordhash");
 
 if (!empty($results)) {
-    $token = hash("sha256", $data->username . time());
+    $token = hash("sha256", $username . time());
     $conn->insert("tokens", ["id", "token"], ["'results[0][id]'", "'$token'"]);
     echo json_encode(['token' => $token, 'status' => "success"]);
 }
