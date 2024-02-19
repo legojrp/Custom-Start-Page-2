e20c904ed2b84354b7bfb634d3c4fe26d306135d
legojrp <legojrp@gmail.com>
2024-02-19 17:20:04 -0500

    Update requests.php

--- backend/backend_dev/requests.php
+++ backend/backend_dev/requests.php
@@ -15,7 +15,8 @@ if ($request->token){
     $id = $conn->select("tokens", ["id"], "token = '$token'");
     if (!empty($id)) {
         $data = $conn->select("users", ["settings"], "id = '" . $id[0]["id"] . "'");
-        echo $data[0]["settings"];
+        $json = json_decode($data[0]["settings"]);
+        echo json_encode($json);
         
     }
     else {
