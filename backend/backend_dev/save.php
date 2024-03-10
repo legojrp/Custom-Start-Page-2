d46fddfeaaccb668c7db5877c6dc5b992c62ce51
legojrp <legojrp@gmail.com>
2024-03-09 22:37:03 -0500

    Update save.php

--- backend/backend_dev/save.php
+++ backend/backend_dev/save.php
@@ -1,6 +1,8 @@
 <?php 
 header('Access-Control-Allow-Origin: *'); // Replace * with your specific origin if needed
-
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header ('Content-Type: application/json');
 
 require_once('DBConnect.php');
 require_once("Credentials.php");  
